f7b00ad71797215ca05d2e740a4c32eef6d67abb
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2021-04-21 19:46:41 -0300

    Update Alias fish

--- FISH/config.fish
+++ FISH/config.fish
@@ -9,16 +9,17 @@ alias rcd="ranger"
 
 # git
 alias gst="git status"
+alias gad="git add"
 alias gc="git commit"
+alias gcl="git clone"
 alias gb="git branch"
-alias gd="git diff"
-alias gco="git checkout"
+alias gck="git checkout"
 alias gp="git push"
 
 # firefox
 alias fire="firefox --private-window"
 
-alias npmisav="npm i --save"
-alias npmidev="npm i --save-dev"
+alias npmi-s="npm i --save"
+alias npmi-dv="npm i --save-dev"
 
 alias ll="ls -la"
