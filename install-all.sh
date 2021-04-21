eb9111235097135336026b1eb2ca58ae56b86177
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2021-04-21 19:58:06 -0300

    Update shel install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -3,15 +3,26 @@
 echo 'OQUE ATUALIZAR?'
 echo '/t [1] VIM'
 echo '/t [2] FISH TERMINAL'
+echo '/t [3] ALL'
 echo '/t [x] nothing'
 echo -n '>'; read update;
 echo ''
 
-if [ $update = 1 ]; then
+function vimInstall(){
 	cd VIM/
 	bash install.sh
-elif [ $update = 2 ]; then 
+}
+
+function fishInstall(){
 	cd FISH/
 	bash install.sh
-fi
+}
 
+if [ $update = 1 ]; then
+	vimInstall
+elif [ $update = 2 ]; then 
+	fishInstall
+elif [ $update = 3 ]; then 
+	vimInstall
+	fishInstall
+fi
