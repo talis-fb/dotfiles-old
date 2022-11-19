3298645a0f92966dfaa67e939d3a7a7e9d387a64
talis-fb <talisonf11@gmail.com>
2022-11-18 23:03:42 -0300

    Update formatter (comment unused code)

--- neovim/.config/nvim/lua/formatter.lua
+++ neovim/.config/nvim/lua/formatter.lua
@@ -1,18 +1,24 @@
-local  null_ls = require('null-ls')
+local null_ls = require('null-ls')
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
-  on_attach = require('lsp').on_attach,
-  sources = {
-    formatting.autopep8,
-    formatting.prettier,
-    formatting.jq,
-    diagnostics.flake8,
-    code_actions.gitsigns,
-  }
+    on_attach = require('lsp').on_attach,
+    sources = {
+        -- ## With Mason this code is not necessary
+        -- -- Diagnostics
+        -- diagnostics.flake8,
+        -- diagnostics.commitlint,
+        -- diagnostics.eslint,
+        --
+        -- -- Format
+        -- formatting.autopep8,
+        -- formatting.prettier,
+        -- formatting.jq,
+        --
+        -- -- Code Actions
+        -- code_actions.gitsigns,
+    }
 })
-
-
